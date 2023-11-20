7f42ce3040955ace089f399118719308415bc685
AndrewOliver616 <andre.oliveira.backup@outlook.com>
2023-11-19 23:53:55 -0300

    Enviando instanciação da classes e teste de casos para as instanciações

--- src/index.js
+++ src/index.js
@@ -1,20 +1,30 @@
 class heroDungeon{
     constructor(nameHero, ageHero, typeHero) {
-        this.name = nameHero
+        this.nameHero = nameHero
         this.ageHero = ageHero
         this.typeHero = typeHero
     }
     
-    toAtacck(typeHero) {
-        if (typeHero == "Wizard") {
-            console.log(`${typeHero} used magic!`)
-        } else if (typeHero == "Warrior") {
-            console.log(`${typeHero} used sword!`)
-        } else if(typeHero == "Monk"){
-            console.log(`${typeHero} used martial arts!`)
-        }else if(typeHero == "Ninja"){
-            console.log(`${typeHero} used shuriken!`)
+    toAtacck() {
+        if (this.typeHero == "Wizard") {
+            console.log(`${this.typeHero} ${this.nameHero} used magic!`)
+        } else if (this.typeHero == "Warrior") {
+            console.log(`${this.typeHero} ${this.nameHero} used sword!`)
+        } else if(this.typeHero == "Monk"){
+            console.log(`${this.typeHero} ${this.nameHero} used martial arts!`)
+        }else if(this.typeHero == "Ninja"){
+            console.log(`${this.typeHero} ${this.nameHero} used shuriken!`)
         }
     }
 
 }
+
+let wizardHero = new heroDungeon("Mago do CSharp", "1000 anos", "Wizard")
+let warriorHero = new heroDungeon("Gladiador do Java", "40 anos", "Warrior")
+let monkHero = new heroDungeon("Mestre do Flutter", "300 anos", "Monk")
+let ninjaHero = new heroDungeon("KunGo","25 anos","Ninja")
+
+wizardHero.toAtacck()
+warriorHero.toAtacck()
+monkHero.toAtacck()
+ninjaHero.toAtacck()
